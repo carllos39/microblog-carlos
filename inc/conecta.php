36b912575b2ef0078b330f1388dfb108b35a7b69
carllos39 <carllos2005@outlook.com.br>
2023-11-16 16:23:54 -0300

    conexão

---
+++ inc/conecta.php
@@ -0,0 +1,16 @@
+<?php 
+/*Script de conexão ao servidor de banco de dados */
+$servidor="localhost";
+$usuario="root";
+$senha="";
+$banco="microblog_carlos";
+/* usando a função mysqli_connect para conectar ao servidor de banco de dados */
+$conexao=mysqli_connect($servidor,$usuario,$senha,$banco);
+mysqli_set_charset($conexao ,"utf8");
+if(!$conexao){
+    die("Deu ruim:".mysqli_connect());
+
+}else{
+echo"Beleza,conectado!";
+}
+?>
