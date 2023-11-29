52615dae73c0119bb8605001b42436f12aa66606
carllos39 <carllos2005@outlook.com.br>
2023-11-29 08:06:01 -0300

    correção

--- inc/funcoes-sessao.php
+++ inc/funcoes-sessao.php
@@ -13,7 +13,7 @@ function verificaAcesso(){
     //se não existir uma variável de sessão chamada  "id "baseada no id  de usuário logado ,então 
     //significa que ele /ela não esta logado(a) no sistema.
     if(! isset($_SESSION['id'])){
-        //Portanto destrua  ods dados  de sessão ,redirecione  spara a página  login .php e pare o script.
+        //Portanto destrua  os dados  de sessão ,redirecione  para a página  login .php e pare o script.
         session_destroy();
         header("location:../login.php?acesso_negado");
         exit;//ou die()
@@ -22,14 +22,22 @@ function verificaAcesso(){
 
 function login($id,$nome,$tipo){
 //Criar variáveis de sessão são recursos que ficam disponível para uso durante  toda  duração  da sessão,ou seja ,enquanto o navegador não for fechado  ou  o usuário não clicar em sair.
-$_SESSION['id']=$id;
-$_SESSION['nome']=$nome;
-$_SESSION['tipo']=$tipo;
+$_SESSION["id"]=$id;
+$_SESSION["nome"]=$nome;
+$_SESSION["tipo"]=$tipo;
 }
 
 function logout(){
     session_destroy();
-    header("location:../login.php?sair");
+    header("location:../login.php?saiu");
     exit;
+}
+function verificarTipo(){
+    // Se tipo de usuário logado na sessão não for admin
+    if($_SESSION['tipo'!='admin']){
+        header("location:nao-autorizado.php");
+        exit;
+
+    }
 }
 ?>
